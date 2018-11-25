0af965140343cac5b7b9fdee0efea98bd9bda2bf
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-11-25 07:31:32 -0500

    html routes

---
+++ app/routing/htmlroutes.js
@@ -0,0 +1,23 @@
+// Dependencies
+var path = require("path");
+
+// Routes
+// ==============================================
+
+// Basic route that sends user to the first page
+module.exports = function (app) {
+  
+// HTML GET REQUESTS
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+  res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+
+
+};
+
+
--- server.js
+++ server.js
@@ -6,34 +6,23 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//FriendFinder Data
-// ==============================================
-var friends = {
 
-};
+// ROUTER
+// =======================================
 
-// Routes
-// ==============================================
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlroutes")(app);
 
-// Basic route that sends user to the first page
-app.get("/", function (req, res) { 
-  res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-
-
-
-
-
-
-
+// =======================================
+// LISTENER
 // Starts the server to begin listening
-// =============================================================
+// =======================================
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
